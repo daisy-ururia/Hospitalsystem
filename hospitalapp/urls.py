9ecc3c099c0f8d3d4015c92b808efc330b07160e
daisy-ururia <uluriadaisy@gmail.com>
2025-02-25 19:02:37 +0300

    initial commit

---
+++ hospitalapp/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+from hospitalapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('starter/', views.starter, name='starter'),
+    path('about/', views.about, name='about'),
+    path('service/', views.service, name='service'),
+    path('departments/', views.departments, name='departments'),
+    path('doctors/', views.doctors, name='doctors'),
+
+]
---
+++ hospitalapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def starter(request):
+    return render(request, 'starter-page.html')
+def about(request):
+    return render(request, 'about.html')
+def service(request):
+    return render(request, 'service.html')
+def departments(request):
+    return render(request, 'departments.html')
+def doctors(request):
+    return render(request, 'doctors.html')
